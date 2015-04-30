c5438f1833a022a72d60b2883d26a03e7b51606c
balakumar-s <s.balakumar@outlook.com>
2015-04-29 18:34:07 -0600

    added max range threshold to cut off laser

--- src/lidar_improved.cpp
+++ src/lidar_improved.cpp
@@ -7,8 +7,9 @@
 #include <std_msgs/Float32.h>
 //Defining constants
 #define PI 3.14
-#define USER_FORCE 0.45 // Change this to priortize goal
-#define ROBOT_RADIUS 0.5 // Change this to robot's dimensions.
+#define USER_FORCE 0.5 // Change this to priortize goal
+#define ROBOT_RADIUS 0.2// Change this to robot's dimensions.
+#define MAX_RANGE 0.7
 //Declaring functions
 void callback(const sensor_msgs::LaserScan::ConstPtr&);
 void userCallback(const std_msgs::Float32&);
@@ -33,7 +34,7 @@ int main(int argc,char**argv)
 
 void userCallback(const std_msgs::Float32& user_data)
 {
-	user_input=user_data.data;
+	user_input=user_data.data+90;
 }
 // The algorithm is run inside the below function so that it is run at the speed of the Lidar scanner.
 void callback(const sensor_msgs::LaserScan::ConstPtr& scan_in)
@@ -44,9 +45,9 @@ void callback(const sensor_msgs::LaserScan::ConstPtr& scan_in)
 	for (int i=0;i<total_points;i++)
 	{
 		//Remove inf values from LIDAR data
-		if(scan_filtered.ranges[i]>5)
+		if(scan_filtered.ranges[i]>MAX_RANGE)
 		{		
-			scan_filtered.ranges[i]=5;
+			scan_filtered.ranges[i]=0;
 		}
 		// Make the robot a point by subtracting the robot radius from the reading.
 		if(scan_filtered.ranges[i]>ROBOT_RADIUS)
@@ -54,10 +55,6 @@ void callback(const sensor_msgs::LaserScan::ConstPtr& scan_in)
 		scan_filtered.ranges[i]=scan_filtered.ranges[i]-ROBOT_RADIUS;
 		}
 		// If there are 0 readings, make them the LIDAR sensor's maximum range.
-		if(scan_filtered.ranges[i]==0)
-		{		
-		//	scan_filtered.ranges[i]=5;
-		}
 	}
 	// Variables for potential field are initialized.
 	float force_x=0;
@@ -68,39 +65,49 @@ void callback(const sensor_msgs::LaserScan::ConstPtr& scan_in)
 	// The goal attraction is obtained from the user input subscriber
 	float user_x=user_force*cos(user_input*PI/180);
 	float user_y=user_force*sin(user_input*PI/180);
+	int non_zero_points=1;
 	//Forces are computed for the LIDAR data.
 	for (int i=0;i<total_points;i++)
 	{ 
-	  
-	  float x = (scan_filtered.ranges[i]*cos(i*PI/180));
-	  float y = (scan_filtered.ranges[i]*sin(i*PI/180));
-	  // The scale is inverted to make nearer obstacles have higher force and the scale is reduced to 1.
-	  x=(5-x)/5;
-	  y=(5-y)/5;
-	  force_x+=x;
-	  force_y+=y;
-	  // For computing the direction vector, the forces from lidar data is inverted so that the robot repels the obstacles.
-	  x = (scan_filtered.ranges[i]*cos(i*PI/180));
-	  y = (scan_filtered.ranges[i]*sin(i*PI/180));
-	  x=-x;
-	  y=-y;
-	  _force_x+=x;
-	  _force_y+=y;
+	  if(scan_filtered.ranges[i]>0)
+	  {
+		  float x = (scan_filtered.ranges[i]*cos(i*PI/180));
+		  float y = (scan_filtered.ranges[i]*sin(i*PI/180));
+		  // The scale is inverted to make nearer obstacles have higher force and the scale is reduced to 1.
+
+		  x=(MAX_RANGE-x)/MAX_RANGE;
+		  y=(MAX_RANGE-y)/MAX_RANGE;
+		  force_x+=x;
+		  force_y+=y;
+		  // For computing the direction vector, the forces from lidar data is inverted so that the robot repels the obstacles.
+		  x = (scan_filtered.ranges[i]*cos(i*PI/180));
+		  y = (scan_filtered.ranges[i]*sin(i*PI/180));
+		  x=MAX_RANGE-x;
+		  y=MAX_RANGE-y;
+		  _force_x+=-x;
+		  _force_y+=-y;
+		  non_zero_points++;
+	  }
 
 	}
-	
+	ROS_INFO("x:%0.2f y:%0.2f",force_x,force_y);
+	total_points=non_zero_points;
 	// The resultant direction is calculated	
     theta=atan2((_force_y/total_points)-user_y,(_force_x/total_points)-user_x)*180/PI;
     // The range is shifted to 0-359 degrees.
-    theta=theta+180;
+    theta=theta+90;
+    if(theta<0)
+    {
+    	theta=360+theta;
+    }
     //force vectors are normalized
-    force_x=(force_x/total_points);//+user_x;
-	force_y=(force_y/total_points);//+user_y;
+    force_x=(force_x/total_points)-user_x;//+user_x;
+	force_y=(force_y/total_points)-user_y;//+user_y;
 	//Resultant force vector magnitude is computed
 	force=sqrt(pow(force_x,2)+pow(force_y,2));
     //Force magnitude and direction is published.
     std_msgs::Float32 force_,theta_;
-	force_.data=100*(force)/sqrt(2);
+	force_.data=100*(force)/sqrt(2*MAX_RANGE);
 	theta_.data=theta;
 	f_pub.publish(force_);
 	theta_pub.publish(theta_);
